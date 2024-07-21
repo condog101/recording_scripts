600195f2ae871353e240fdc0745e50f2f97e6173
connorscomputer <cd1723@ic.ac.uk>
2024-07-21 17:14:42 +0100

    force conversion to float64

--- playback_mkv.py
+++ playback_mkv.py
@@ -6,7 +6,6 @@ from pyk4a import PyK4APlayback, ImageFormat
 import open3d as o3d
 
 
-
 def colorize(
     image: np.ndarray,
     clipping_range: Tuple[Optional[int], Optional[int]] = (None, None),
@@ -19,6 +18,7 @@ def colorize(
     img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
     img = cv2.applyColorMap(img, colormap)
     return img
+
 
 def info(playback: PyK4APlayback):
     print(f"Record length: {playback.length / 1000000: 0.2f} sec")
@@ -33,12 +33,16 @@ def play(playback: PyK4APlayback):
         try:
             capture = playback.get_next_capture()
             if capture.color is not None and capture.depth is not None:
-                capture._color = cv2.cvtColor(cv2.imdecode(capture.color, cv2.IMREAD_COLOR), cv2.COLOR_BGR2BGRA)
+                capture._color = cv2.cvtColor(cv2.imdecode(
+                    capture.color, cv2.IMREAD_COLOR), cv2.COLOR_BGR2BGRA)
                 capture._color_format = ImageFormat.COLOR_BGRA32
-                points = capture.depth_point_cloud.reshape((-1, 3))
-                colors = capture.transformed_color[..., (2, 1, 0)].reshape((-1, 3))
+                points = capture.depth_point_cloud.reshape(
+                    (-1, 3)).astype('float64')
+                colors = capture.transformed_color[..., (2, 1, 0)].reshape(
+                    (-1, 3))
                 geometry.points = o3d.utility.Vector3dVector(points)
-                geometry.colors = o3d.utility.Vector3dVector(colors/255)
+                geometry.colors = o3d.utility.Vector3dVector(
+                    (colors/255).astype('float64'))
                 if first:
                     vis.add_geometry(geometry)
                     first = False
@@ -46,17 +50,19 @@ def play(playback: PyK4APlayback):
                     vis.update_geometry(geometry)
                 vis.poll_events()
                 vis.update_renderer()
- 
-            
+
         except EOFError:
             break
-    
-#"/home/connor/trial_recordings/V6TNrEsM_20240529_155036.mkv"
+
+# "/home/connor/trial_recordings/V6TNrEsM_20240529_155036.mkv"
+
 
 def main() -> None:
     parser = ArgumentParser(description="pyk4a player")
-    parser.add_argument("--seek", type=float, help="Seek file to specified offset in seconds", default=0.0)
-    parser.add_argument("FILE", type=str, help="Path to MKV file written by k4arecorder")
+    parser.add_argument(
+        "--seek", type=float, help="Seek file to specified offset in seconds", default=0.0)
+    parser.add_argument(
+        "FILE", type=str, help="Path to MKV file written by k4arecorder")
 
     args = parser.parse_args()
     filename: str = args.FILE
